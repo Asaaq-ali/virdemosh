c22301bdbcb921f06d99e177324dba0521950f5a
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-31 18:07:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ DEVS = ["ASAKIOb","lll_lll89"]
 
 bot_id = bot.bot_token.split(":")[0]
 
-async def start_bot():
+async def start_vgtrebot():
     print("تم تشغيل الصانع بنجاح..💗")
     await bot.start()
     for hh in DEVS:
