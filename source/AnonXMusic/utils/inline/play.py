1997d768c4baa789fcb154fb8be91fb51298be47
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-10-31 16:29:56 +0300

    Add files via upload

---
+++ source/AnonXMusic/utils/inline/play.py
@@ -0,0 +1,336 @@
+import math
+
+import config
+from AnonXMusic import app
+
+from pyrogram.types import InlineKeyboardButton
+from AnonXMusic.utils.formatters import time_to_seconds
+
+
+def track_markup(_, videoid, user_id, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            )
+        ],
+    ]
+    return buttons
+
+
+def stream_markup_timer(_, chat_id, played, dur):
+    played_sec = time_to_seconds(played)
+    duration_sec = time_to_seconds(dur)
+    percentage = (played_sec / duration_sec) * 100
+    umm = math.floor(percentage)
+    if 0 < umm <= 10:
+        bar = "⚡بدء التشغيل⚡"
+    elif 2 < umm < 3:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 3 <= umm < 4:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 4 <= umm < 5:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 6 <= umm < 7:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 7 <= umm < 8:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 9 <= umm < 10:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 11 <= umm < 12:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 12 <= umm < 13:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 13 < umm < 14:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 14 <= umm < 15:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 15 <= umm < 16:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 16 <= umm < 17:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 17 <= umm < 18:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 18 <= umm < 19:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 19 <= umm < 20:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 20 <= umm < 21:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 21 <= umm < 22:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 22 <= umm < 23:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 23 <= umm < 24:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 24 <= umm < 25:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 25 <= umm < 26:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 26 <= umm < 27:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 27 <= umm < 28:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 28 <= umm < 29:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 29 <= umm < 30:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 30 <= umm < 31:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 31 <= umm < 32:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 32 <= umm < 33:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 33 <= umm < 34:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 34 <= umm < 35:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 35 <= umm < 36:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 36 <= umm < 37:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 37 <= umm < 38:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 38 <= umm < 39:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 39 <= umm < 40:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 40 <= umm < 41:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 41 <= umm < 42:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 42 <= umm < 43:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 43 <= umm < 44:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 44 < umm < 45:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 45 <= umm < 46:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 46 <= umm < 47:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 47 <= umm < 48:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 48 <= umm < 49:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 49 <= umm < 50:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 50 <= umm < 51:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 51 <= umm < 52:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 52 <= umm < 53:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 53 <= umm < 54:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 54 <= umm < 55:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 55 <= umm < 56:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 56 <= umm < 57:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 57 <= umm < 58:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 58 <= umm < 59:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 59 <= umm < 60:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 60 <= umm < 61:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 61 <= umm < 62:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 62 <= umm < 63:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 63 <= umm < 64:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 64 <= umm < 65:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 65 <= umm < 66:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 66 <= umm < 67:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 67 <= umm < 68:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 68 <= umm < 69:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 69 <= umm < 70:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 70 <= umm < 71:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 71 <= umm < 72:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 72 <= umm < 73:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 73 <= umm < 74:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 74 <= umm < 75:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 75 <= umm < 76:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 76 < umm < 77:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 77 <= umm < 78:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 78 <= umm < 79:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 79 <= umm < 80:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 80 <= umm < 81:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 81 <= umm < 82:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 82 <= umm < 83:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 83 <= umm < 84:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 84 <= umm < 85:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 85 <= umm < 86:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 86 <= umm < 87:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 87 <= umm < 88:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 88 <= umm < 89:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 89 <= umm < 90:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 90 <= umm < 91:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 91 <= umm < 92:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 92 <= umm < 93:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 93 <= umm < 94:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 94 <= umm < 95:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    elif 95 <= umm < 96:
+        bar = "ﮩ٨ـﮩ©ﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 96 <= umm < 97:
+        bar = "ﮩ©٨ـﮩﮩ٨ـﮩ٨ـﮩﮩ٨ـ"
+    elif 97 <= umm < 98:
+        bar = "ﮩ٨ـﮩﮩ٨ـﮩ٨ـﮩ©ﮩ٨ـ"
+    elif 98 <= umm < 99:
+        bar = "ﮩ٨ـﮩﮩ٨ـ©ﮩ٨ـﮩﮩ٨ـ"
+    else:
+        bar = "—————————◉"
+    buttons = [
+        [
+            InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⏸", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="⏭", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⏹", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+        InlineKeyboardButton(text=_["S_S_G_B_4"], url=config.SUPPORT_CHANNEL)
+        ], 
+        ] 
+    return buttons
+
+
+def stream_markup(_, chat_id):
+    buttons = [
+        [
+            InlineKeyboardButton(text="▶️", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="⏸", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="🔄", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="⏭", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="⏹", callback_data=f"ADMIN Stop|{chat_id}"),
+        ],
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        [
+        InlineKeyboardButton(text=_["S_S_G_B_4"], url=config.SUPPORT_CHANNEL)
+        ], 
+        ]  
+    return buttons
+
+
+def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+
+
+def livestream_markup(_, videoid, user_id, mode, channel, fplay):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_3"],
+                callback_data=f"LiveStream {videoid}|{user_id}|{mode}|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {videoid}|{user_id}",
+            ),
+        ],
+    ]
+    return buttons
+
+
+def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
+    query = f"{query[:20]}"
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["P_B_1"],
+                callback_data=f"MusicStream {videoid}|{user_id}|a|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["P_B_2"],
+                callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="◁",
+                callback_data=f"slider B|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+            InlineKeyboardButton(
+                text=_["CLOSE_BUTTON"],
+                callback_data=f"forceclose {query}|{user_id}",
+            ),
+            InlineKeyboardButton(
+                text="▷",
+                callback_data=f"slider F|{query_type}|{query}|{user_id}|{channel}|{fplay}",
+            ),
+        ],
+    ]
+    return buttons
