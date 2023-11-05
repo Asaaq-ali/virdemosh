ab73f0f4c4a5f52aa1ac4465145e7db4c507582e
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 14:23:26 +0300

    Update logger.py

--- source/AnonXMusic/plugins/sudo/logger.py
+++ source/AnonXMusic/plugins/sudo/logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import add_off, add_on
-from AnonXMusic.utils.decorators.language import language
+from source.AnonXMusic import app
+from source.AnonXMusic.misc import SUDOERS
+from source.AnonXMusic.utils.database import add_off, add_on
+from source.AnonXMusic.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
