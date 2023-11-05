f875745e49537129bf06d50698d512c798130b2d
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 14:28:04 +0300

    Update ping.py

--- source/AnonXMusic/plugins/tools/ping.py
+++ source/AnonXMusic/plugins/tools/ping.py
@@ -3,12 +3,12 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils import bot_sys_stats
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.inline import supp_markup
-from config import BANNED_USERS, PING_IMG_URL
+from source.AnonXMusic import app
+from source.AnonXMusic.core.call import Anony
+from source.AnonXMusic.utils import bot_sys_stats
+from source.AnonXMusic.utils.decorators.language import language
+from source.AnonXMusic.utils.inline import supp_markup
+from source.config import BANNED_USERS, PING_IMG_URL
 
 
 @app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
