e9022d6ae8a5a662353704eb9d6279762f54127f
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 14:26:56 +0300

    Update dev.py

--- source/AnonXMusic/plugins/tools/dev.py
+++ source/AnonXMusic/plugins/tools/dev.py
@@ -10,8 +10,8 @@ from time import time
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from AnonXMusic import app
-from config import OWNER_ID
+from source.AnonXMusic import app
+from source.config import OWNER_ID
 
 
 async def aexec(code, client, message):
