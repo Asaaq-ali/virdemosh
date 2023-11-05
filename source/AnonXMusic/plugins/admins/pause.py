c30d4412798409333487aaf838a0ee1dce45cddf
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 05:12:04 +0300

    Update pause.py

--- source/AnonXMusic/plugins/admins/pause.py
+++ source/AnonXMusic/plugins/admins/pause.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
-from config import BANNED_USERS
+from source.AnonXMusic import app
+from source.AnonXMusic.core.call import Anony
+from source.AnonXMusic.utils.database import is_music_playing, music_off
+from source.AnonXMusic.utils.decorators import AdminRightsCheck
+from source.AnonXMusic.utils.inline import close_markup
+from source.config import BANNED_USERS
 
 
 @app.on_message(filters.command(["دينا اسكت", "اسكت"],"") & filters.group & ~BANNED_USERS)
