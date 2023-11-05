58e0b94f1f142678c92b6f9bc6545e7168111a78
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 05:43:21 +0300

    Update stream.py

--- source/AnonXMusic/utils/stream/stream.py
+++ source/AnonXMusic/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from AnonXMusic import Carbon, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import AnonyBin
-from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
-from AnonXMusic.utils.thumbnails import get_thumb
+from source.AnonXMusic import Carbon, YouTube, app
+from source.AnonXMusic.core.call import Anony
+from source.AnonXMusic.misc import db
+from source.AnonXMusic.utils.database import add_active_video_chat, is_active_chat
+from source.AnonXMusic.utils.exceptions import AssistantErr
+from source.AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
+from source.AnonXMusic.utils.pastebin import AnonyBin
+from source.AnonXMusic.utils.stream.queue import put_queue, put_queue_index
+from source.AnonXMusic.utils.thumbnails import get_thumb
 
 
 async def stream(
