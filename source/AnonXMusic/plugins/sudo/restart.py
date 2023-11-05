c1729ad43a7c5c9ce0fbc1c6158e2954a29f3c27
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 14:25:16 +0300

    Update restart.py

--- source/AnonXMusic/plugins/sudo/restart.py
+++ source/AnonXMusic/plugins/sudo/restart.py
@@ -10,15 +10,15 @@ from git.exc import GitCommandError, InvalidGitRepositoryError
 from pyrogram import filters
 
 import config
-from AnonXMusic import app
-from AnonXMusic.misc import HAPP, SUDOERS, XCB
-from AnonXMusic.utils.database import (
+from source.AnonXMusic import app
+from source.AnonXMusic.misc import HAPP, SUDOERS, XCB
+from source.AnonXMusic.utils.database import (
     get_active_chats,
     remove_active_chat,
     remove_active_video_chat,
 )
-from AnonXMusic.utils.decorators.language import language
-from AnonXMusic.utils.pastebin import AnonyBin
+from source.AnonXMusic.utils.decorators.language import language
+from source.AnonXMusic.utils.pastebin import AnonyBin
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
