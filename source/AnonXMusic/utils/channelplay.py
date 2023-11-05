5c61e1fd093c0568ecefcf31998322bd74f61b64
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 05:34:11 +0300

    Update channelplay.py

--- source/AnonXMusic/utils/channelplay.py
+++ source/AnonXMusic/utils/channelplay.py
@@ -1,5 +1,5 @@
-from AnonXMusic import app
-from AnonXMusic.utils.database import get_cmode
+from source.AnonXMusic import app
+from source.AnonXMusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
