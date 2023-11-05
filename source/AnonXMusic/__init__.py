e355d95341402b06213309c08b9de409f9a50242
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 04:39:32 +0300

    Update __init__.py

--- source/AnonXMusic/__init__.py
+++ source/AnonXMusic/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from .AnonXMusic.core.bot import Anony
+from .AnonXMusic.core.dir import dirr
+from .AnonXMusic.core.git import git
+from .AnonXMusic.core.userbot import Userbot
+from .AnonXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
