e330ea79f74a66bddab14bf62f7d04169819981d
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 05:35:00 +0300

    Update extraction.py

--- source/AnonXMusic/utils/extraction.py
+++ source/AnonXMusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from AnonXMusic import app
+from source.AnonXMusic import app
 
 
 async def extract_user(m: Message) -> User:
