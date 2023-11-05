c5959246154d825fa2dd44b310c51a897b212276
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 05:09:22 +0300

    Update Youtube.py

--- source/AnonXMusic/platforms/Youtube.py
+++ source/AnonXMusic/platforms/Youtube.py
@@ -18,8 +18,8 @@ from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from source.AnonXMusic.utils.database import is_on_off
+from source.AnonXMusic.utils.formatters import time_to_seconds
 
 
 async def shell_cmd(cmd):
