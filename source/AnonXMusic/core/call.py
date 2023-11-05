3d948b5e62cadef8ad19ea347b8321b18794d4c0
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 05:08:06 +0300

    Update call.py

--- source/AnonXMusic/core/call.py
+++ source/AnonXMusic/core/call.py
@@ -17,9 +17,9 @@ from pytgcalls.types.input_stream.quality import HighQualityAudio, MediumQuality
 from pytgcalls.types.stream import StreamAudioEnded
 
 import config
-from AnonXMusic import LOGGER, YouTube, app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import (
+from source.AnonXMusic import LOGGER, YouTube, app
+from source.AnonXMusic.misc import db
+from source.AnonXMusic.utils.database import (
     add_active_chat,
     add_active_video_chat,
     get_lang,
@@ -31,12 +31,12 @@ from AnonXMusic.utils.database import (
     remove_active_video_chat,
     set_loop,
 )
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
-from AnonXMusic.utils.inline.play import stream_markup
-from AnonXMusic.utils.stream.autoclear import auto_clean
-from AnonXMusic.utils.thumbnails import get_thumb
-from strings import get_string
+from source.AnonXMusic.utils.exceptions import AssistantErr
+from source.AnonXMusic.utils.formatters import check_duration, seconds_to_min, speed_converter
+from source.AnonXMusic.utils.inline.play import stream_markup
+from source.AnonXMusic.utils.stream.autoclear import auto_clean
+from source.AnonXMusic.utils.thumbnails import get_thumb
+from source.strings import get_string
 
 autoend = {}
 counter = {}
