01a7a01b74940d5313d4dcfdfadcc9987762642b
vidroasaac <148828173+Asaaq-ali@users.noreply.github.com>
2023-11-05 05:30:06 +0300

    Update watcher.py

--- source/AnonXMusic/plugins/misc/watcher.py
+++ source/AnonXMusic/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
+from source.AnonXMusic import app
+from source.AnonXMusic.core.call import Anony
 
 welcome = 20
 close = 30
